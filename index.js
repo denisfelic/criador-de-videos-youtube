ac71c2e7329aa17f151d667e1e21dad0be7b6f0e
Denis F <denisf.salles@gmail.com>
2020-05-18 19:46:37 -0300

    feat: Implementada api de busca de imagens do Google

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 const userInput = require('./robots/user-input.js');
 const textRobot = require('./robots/text-robot.js');
+const imageRobot = require('./robots/image-robot.js');
+const stateFile = require('./robots/state-save-robot');
 
 const robots = {
   userInput,
   textRobot,
+  imageRobot,
 };
 
 async function start() {
-  const content = {};
+  content = await stateFile.load();
+  if(content){
+  let content = {};
+  stateFile.save(content);
+  }
 
+  
+  
   robots.userInput(content);
   await robots.textRobot();
-  // const loaded = robots.stateSaveRobot.load();
-  // console.log(JSON.stringify(content, null, 4));
+  await robots.imageRobot();
+  content = await stateFile.load();
+  console.dir(content, { depth: null });
+   // console.log(JSON.stringify(content, null, 4));
 }
 
 start();
